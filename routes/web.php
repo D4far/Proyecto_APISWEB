c9d1e91606460827098f8b4c2acc8d94994aa6bd
D4far <danyreina2005@gmail.com>
2024-11-23 09:17:14 -0500

    Rutas actualizadas y web.php arrreglado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\productoController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
